61964524ec8ef52dc1b44300ab618f8248d6933a
User <User@HP>
2022-04-19 11:42:43 +0530

    aaaa

---
+++ capgemini/src/main/java/com/cap/capgemini/App.java
@@ -0,0 +1,15 @@
+package com.cap.capgemini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	
+        System.out.println( "Hello World!" );
+    }
+}
